2bf1ef302c68988f441129d83a15568b0195ce07
dromerolovo <dromerolovo@gmail.com>
2025-07-15 15:31:35 -0700

    experience-detail

--- csis3275/src/main/java/com/example/csis3275/controllers/ExperienceController.java
+++ csis3275/src/main/java/com/example/csis3275/controllers/ExperienceController.java
@@ -5,12 +5,10 @@ import com.example.csis3275.repositories.ExperienceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/experience")
@@ -32,4 +30,16 @@ public class ExperienceController {
         model.addAttribute("experiences", experiences);
         return "experience-search";
     }
+
+    @GetMapping("/detail/{id}")
+    public String experienceDetailGet(@PathVariable int id, Model model) {
+        Optional<Experience> experience = experienceRepository.findById((long) id);
+
+        if (experience.isPresent()) {
+            model.addAttribute("experience", experience.get());
+            return "experience-detail";
+        } else {
+            return "redirect:/experience/search";   
+        }
+    }    
 }
